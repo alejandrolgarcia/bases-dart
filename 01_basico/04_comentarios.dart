7d1e6c54750ef330ee56f046a92247fe7b6f28c3
Luis Garcia <lalejandro.1988@gmail.com>
2022-08-03 21:27:17 -0600

    comentarios en dart

---
+++ 01_basico/04_comentarios.dart
@@ -0,0 +1,33 @@
+
+/**
+ * Este archivo sirve para explicar los comentarios en dart
+ * 
+ * [creadoEn]: 03/08/2022
+ */
+
+main() {
+
+    // Esta es una variable temporal que queremos definir para ejemplificar comentarios
+    int a = 10;
+
+    /*
+    final personas = [
+        // Esta es la primera linea
+        'Alejandro', // posicion [0]
+        // Esta es la segunda linea
+        'Celeste', // posicion [1]
+        // Esta es la tercera linea
+        'Ariana' // posicion [2]
+    ];
+    */
+
+}
+
+/// La función division permite dividir dos números
+///
+/// recibe como parametros el [dividendo] y el [divisor]
+///
+
+division( double dividendo, double divisor) {
+
+}
