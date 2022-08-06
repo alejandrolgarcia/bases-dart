61126553d3c9dfb5d5c8fee6243b198f0e74275c
Luis Garcia <lalejandro.1988@gmail.com>
2022-08-06 15:46:26 -0600

    Break y continue, for con etiquetas y Switch

---
+++ 02_control/07_break_continue.dart
@@ -0,0 +1,18 @@
+
+main() {
+
+    for (int i = 0; i < 10; i++) {
+        
+        if ( i == 4 ) {
+            continue;
+        }
+
+        print(i);
+
+        if ( i == 7 ) {
+            break;
+        }
+
+    }
+
+}
